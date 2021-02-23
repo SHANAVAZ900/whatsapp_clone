dae54450b10f6172601688e1d1eb04cd71eded83
zamindar-786 <zamindar900@gmail.com>
2021-02-24 01:30:01 +0530

    added sidebar icons,firebase

---
+++ whatsapp/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyARGtZWHlczihJMmXXUE6bj7yfPgLAg6Q0",
+    authDomain: "whatsapp-67923.firebaseapp.com",
+    projectId: "whatsapp-67923",
+    storageBucket: "whatsapp-67923.appspot.com",
+    messagingSenderId: "546705878141",
+    appId: "1:546705878141:web:01b45fdb48931c1bbe1514",
+    measurementId: "G-PYBXH9LF8W"
+  };
