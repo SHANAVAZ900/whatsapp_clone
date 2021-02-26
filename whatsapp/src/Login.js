e911b065a22d53e3438c97280bb937b616740eb5
zamindar-786 <zamindar900@gmail.com>
2021-02-27 02:02:20 +0530

    created a login page with authentication

--- whatsapp/src/Login.js
+++ whatsapp/src/Login.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import {Button} from "@material-ui/core";
 import "./Login.css";
+import{ auth , provider } from "./firebase"
+
+
+
 function Login() {
+    const signIn =() =>{
+        auth.signInWithPopup(provider)
+        .then(result => console.log(result))
+        .catch((error) => alert(error.message));
+        
+
+    };
+
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="login">
+            <div className="login__container">
+                <img 
+              src="https://media.glassdoor.com/sqll/385763/msit-squarelogo-1447831288116.png" alt=""
+                />
+              <div className="login__text">
+                  <h1>Sign in to MSIT Chat app</h1>
+              </div>
+              <Button  onClick={signIn}>
+                    Sign In with Google
+              </Button>
+            </div>
+            
         </div>
     )
 }
