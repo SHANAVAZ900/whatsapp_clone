2f098b382825e299a886c89c67fd505efb80ac90
zamindar-786 <zamindar900@gmail.com>
2021-02-26 11:35:03 +0530

    added new chat to prompt on the screen

--- whatsapp/src/SidebarChat.js
+++ whatsapp/src/SidebarChat.js
@@ -1,23 +1,36 @@
-import { Avatar } from '@material-ui/core';
+import { Avatar, Portal } from '@material-ui/core';
 import React, { useEffect, useState} from 'react';
 import './SidebarChat.css';
 
 
-function SidebarChat() {
+function SidebarChat({ addNewChat }) {
     const [seed, setSeed]= useState('');
     
     useEffect(() => {
         setSeed(Math.floor(Math.random()* 5000));
     }, []);
 
-    return (
+    const createChat = () => {
+        const roomName = prompt("please enter name for chat");
+
+        if (roomName){
+            //need tp add stuff
+
+        }
+    };
+    return !addNewChat ? (
         <div className="sidebarChat">
-             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
             <div classname="sidebarChat__info">
                 <h2>Room name</h2>
                 <p>Last mesaage...</p>
             </div>
         </div>
+    ): (
+       <div onclick={ createChat } 
+       className="sidebarChat">
+           <h2>Add New Chat</h2>
+       </div>
     )
 }
 
