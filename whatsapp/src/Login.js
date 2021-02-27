e55dd2ae10b9e66007d5161af73afb1efc5fba93
zamindar-786 <zamindar900@gmail.com>
2021-02-27 22:48:29 +0530

    added the google sign-in page

--- whatsapp/src/Login.js
+++ whatsapp/src/Login.js
@@ -2,17 +2,25 @@ import React from 'react';
 import {Button} from "@material-ui/core";
 import "./Login.css";
 import{ auth , provider } from "./firebase"
+import {actionTypes} from "./reducer";
+import { useStateValue } from './StateProvider';
 
 
 
 function Login() {
-    const signIn =() =>{
-        auth.signInWithPopup(provider)
-        .then(result => console.log(result))
-        .catch((error) => alert(error.message));
-        
-
-    };
+    
+    const [{}, dispatch] = useStateValue();
+    const signIn = () => {
+        auth
+            .signInWithPopup(provider)
+            .then((result) => {
+                dispatch({
+                    type: actionTypes.SET_USER,
+                    user: result.user,
+                })
+            })
+            .catch((error) => alert(error.message));
+    }
 
 
     return (
